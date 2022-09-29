79acdb67572d900698bc4bd106dc5863e1ce05b5
Grig <chiefy@mail.ru>
2022-09-29 15:44:05 +0200

    Add Home Works Codes

--- HomeWork_Task66/Program.cs
+++ HomeWork_Task66/Program.cs
@@ -2,3 +2,23 @@
 // сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Clear();
+
+int m = InputNumber("Pls enter M: ");
+int n = InputNumber("Pls enter N: ");
+Console.WriteLine($"M = {m}; N = {n} -> {CountSumNaturalElements(m, n)}");
+
+
+int InputNumber(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+
+int CountSumNaturalElements(int m, int n)
+{
+    if (m == n) return n;
+    return n + CountSumNaturalElements(m, n - 1);
+}
--- HomeWork_Task68/Program.cs
+++ HomeWork_Task68/Program.cs
@@ -2,3 +2,24 @@
 // рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+Console.WriteLine("Pls enter two positive numers: M и N.");
+int m = InputNumber("m = ");
+int n = InputNumber("n = ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+
+int InputNumber(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
